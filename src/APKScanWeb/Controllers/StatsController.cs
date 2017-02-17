24796164e0fbb22900f775ee044075f97caf3480
NVitanovic <x-team@live.com>
2017-02-17 19:22:59 +0100

    Added Stats model and controller. Now we can add any JSON like object to MongoDB specific collections for later use. Two functions are available one for adding to scan collection and other for adding to result collection, so we can track on which page what data has been collected about the user

---
+++ src/APKScanWeb/Controllers/StatsController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using APKScanWeb.Models;
+using System.IO;
+
+namespace APKScanWeb.Controllers
+{
+    [Route("api/[controller]")]
+    public class StatsController : Controller
+    {
+        private Stats statsModel = new Stats();
+        public String Index()
+        {
+            return "";
+        }
+        private JsonResult addToCollection(string collection)
+        {
+            var stream = Request.Body;
+            StreamReader reader = new StreamReader(stream);
+            string data = reader.ReadToEnd();
+            if (!statsModel.addToScanCollection(data))
+                return Json(new { success = false });
+            return Json(new { success = true });
+        }
+        [HttpPost("scan")]
+        public JsonResult addToScan()
+        {
+            return addToCollection("scan");
+        }
+        [HttpPost("result")]
+        public JsonResult addToResult()
+        {
+            return addToCollection("result");
+        }
+    }
+}
---
+++ src/APKScanWeb/Models/Stats.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MongoDB;
+using MongoDB.Bson;
+
+namespace APKScanWeb.Models
+{
+    public class Stats
+    {
+        private DataLayer dl;
+        public Stats()
+        {
+            try
+            {
+                dl = DataLayer.getInstance();
+            }
+            catch
+            {
+                throw new Exception("Error while getting DataLayer instance!");
+            }
+        }
+        private bool addToCollection(string data, string collection)
+        {
+            if (String.IsNullOrEmpty(data))
+                return false;
+
+            try
+            {
+                var bsonDoc = MongoDB.Bson.Serialization.BsonSerializer.Deserialize<BsonDocument>(data);
+                var mongoCol = dl.mongo.GetCollection<BsonDocument>("scan");
+                mongoCol.InsertOne(bsonDoc);
+            }
+            catch
+            {
+                return false;
+            }
+   
+            return true;
+        }
+        public bool addToScanCollection(string data)
+        {
+            return addToCollection(data, "scan");
+        }
+        public bool addToResultCollection(string data)
+        {
+            return addToCollection(data, "result");
+        }
+    }
+}
